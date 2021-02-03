34d7ac60d88ee1948915a42d211465db0d401f86
Michael <css.nigeria@gmail.com>
2021-02-03 12:12:21 +0100

    added folder content lister

---
+++ src/com/techline/ReadFolderContent.java
@@ -0,0 +1,22 @@
+package com.techline;
+import java.io.File;
+
+public class ReadFolderContent {
+    public static void main(String[] args){
+        // creates a file object
+        File file = new File("/home/mike/Documents/personal/NODE_PROJECTS/ELECTRON_ROJECTS/First_Approach/SchoolLine/src/htmlpages");
+
+        // returns an array of all files
+        String[] fileList = file.list();
+
+        if (fileList != null) {
+            for(String str : fileList) {
+                System.out.println(str);
+            }
+        }else{
+            System.out.println("file list is null");
+        }
+    }
+
+
+}
